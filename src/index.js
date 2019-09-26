d3d7e11a5bec1c28b8a228d33e5b3d8ec4292543
Zak Laughton <zlaughton@atlassian.com>
2019-09-26 17:10:15 -0500

    style: clean up code

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 const AppRouter = () => (
   <BrowserRouter>
-    <Route
-      exact
-      path='/:parameter1/:parameter2/:parameter3'
-      // "..." spread operator passes in each property of props as a separate parameter:
-      render={props => <App {...props} />}
-    />
+    <Route exact path='/:parameter1/:parameter2/:parameter3' render={props => <App {...props} />} />
   </BrowserRouter>
 );
 
-// "{ match }" pulls out just the "match" parameter from the props
 const App = props => {
-  // destructure each property from params into its own variable to avoid having to type "match.params"
-  // before each variable
   const { parameter1, parameter2, parameter3 } = props.match.params;
   return (
     <div>
