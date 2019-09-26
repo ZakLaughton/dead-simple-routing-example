dbae8917a98961a9692abd315338f4a9c7f76901
Zak Laughton <zlaughton@atlassian.com>
2019-09-26 17:01:44 -0500

    initial work commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppRouter = () => (
+  <BrowserRouter>
+    <Route
+      exact
+      path='/:parameter1/:parameter2/:parameter3'
+      // "..." spread operator passes in each property of props as a separate parameter:
+      render={props => <App {...props} />}
+    />
+  </BrowserRouter>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// "{ match }" pulls out just the "match" parameter from the props
+const App = props => {
+  // destructure each property from params into its own variable to avoid having to type "match.params"
+  // before each variable
+  const { parameter1, parameter2, parameter3 } = props.match.params;
+  return (
+    <div>
+      First: {parameter1} <br />
+      Second: {parameter2} <br />
+      Third: {parameter3} <br />
+    </div>
+  );
+};
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
